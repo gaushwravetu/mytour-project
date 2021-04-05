ec9169bdaac20dc8955363d0cb47bfb982913f0c
gauravappy97 <gauravappy1997@gmail.com>
2021-04-05 15:06:15 +0530

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\gaurav\visual studio\django\mytour-project\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
