3f702fb226680f36e1e0f3f2b95a95ee1aad5122
gauravappy97 <gauravappy1997@gmail.com>
2021-04-08 09:53:55 +0530

    worked on home and about

--- pages/urls.py
+++ pages/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home,name='home'),
+    path('about',views.about,name='about'),
+    path('community',views.community,name='community'),
+    path('places',views.places,name='places'),
+    path('contact',views.contact,name='contact')
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 def home(request):
     return render(request,'pages/home.html')
 
+def about(request):
+    return render(request,'pages/about.html')
+
+def community(request):
+    return render(request,'pages/community.html')
+
+def places(request):
+    return render(request,'pages/places.html')
+
+def contact(request):
+    return render(request,'pages/contact.html')
