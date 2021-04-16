07874498cfa123b5e986952dd3031c115a1c7cb0
gauravappy97 <gauravappy1997@gmail.com>
2021-04-17 00:02:28 +0530

    set up dedicated car page

--- place/urls.py
+++ place/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.place,name='place'),
-    
+    path('<int:id>',views.place_detail,name='place_detail')
 ]
--- place/views.py
+++ place/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Place
 # Create your views here.
 def place(request):
     return render(request,'place/place.html')
+
+def place_detail(request, id):
+    single_place = get_object_or_404(Place,pk=id)
+
+    data = {
+        'single_place': single_place,
+    }
+    return render(request,'place/place_detail.html',data)
