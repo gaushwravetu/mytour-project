2083a93dd6b0bb74864659995600621bec81a9b2
gauravappy97 <gauravappy1997@gmail.com>
2021-04-17 23:12:48 +0530

    worked on pagination and db

--- place/views.py
+++ place/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Place
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 def place(request):
-    return render(request,'place/place.html')
+    places = Place.objects.order_by('-added_date')
+    paginator = Paginator(places,4)
+    page = request.GET.get('page')
+    paged_places = paginator.get_page(page)
+    data = {
+        'places': paged_places,
+    }
+    return render(request,'place/place.html',data)
 
 def place_detail(request, id):
     single_place = get_object_or_404(Place,pk=id)
